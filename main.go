cf574823b9d975bcbe490f3354bffcbc3b7145db
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-15 10:42:39 +0700

    feat: add word count functionality to main program

--- main.go
+++ main.go
@@ -8,5 +8,17 @@ import (
 func main() {
 	data, _ := os.ReadFile("./words.txt")
 
+	wordCount := 0
+
+	for _, x := range data {
+		if x == ' ' {
+			wordCount += 1
+		}
+	}
+
+	wordCount++
+
+	fmt.Println(wordCount)
+
 	fmt.Println("data:", string(data))
 }
