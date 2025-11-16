776459c83d71b5928d20a6e53418dad97fb65544
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-16 18:50:15 +0700

    refactor: improve word count logic and enhance test cases

--- main.go
+++ main.go
@@ -18,12 +18,19 @@ func CountWords(data []byte) int {
 		return 0
 	}
 
+	wordDetected := false
 	wordCount := 0
 
 	for _, x := range data {
 		if x == ' ' {
 			wordCount++
+		} else {
+			wordDetected = true
 		}
+	}
+
+	if !wordDetected {
+		return 0
 	}
 
 	wordCount++
--- main_test.go
+++ main_test.go
@@ -3,33 +3,45 @@ package main
 import "testing"
 
 func TestCountWords(t *testing.T) {
-	input := "one two three four five"
-	wants := 5
-
-	result := CountWords([]byte(input))
-
-	if result != wants {
-		t.Logf("expected: %v, got: %v", wants, result)
-		t.Fail()
+	testCases := []struct {
+		name  string
+		input string
+		wants int
+	}{
+		{
+			name:  "5 words,",
+			input: "one two three four five",
+			wants: 5,
+		},
+		{
+			name:  "empty input",
+			input: "",
+			wants: 0,
+		},
+		{
+			name:  "single space",
+			input: " ",
+			wants: 0,
+		},
+		{
+			name:  "new lines",
+			input: "one two three\nfour five",
+			wants: 5,
+		},
+		{
+			name:  "multiple spaces",
+			input: "This is a sentence.  This is another",
+			wants: 7,
+		},
 	}
 
-	input = ""
-	wants = 0
-
-	result = CountWords([]byte(input))
-
-	if result != wants {
-		t.Logf("expected: %v, got: %v", wants, result)
-		t.Fail()
-	}
-
-	input = " "
-	wants = 0
-
-	result = CountWords([]byte(input))
-
-	if result != wants {
-		t.Logf("expected: %v, got: %v", wants, result)
-		t.Fail()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := CountWords([]byte(tc.input))
+			if result != tc.wants {
+				t.Logf("expected: %v, got: %v", tc.wants, result)
+				t.Fail()
+			}
+		})
 	}
 }
