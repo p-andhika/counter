e5bc5dfd8aa6fa3776dade6c54c802f39e3a12b3
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-11-19 14:06:53 +0700

    refactor: replace ReadFile with streaming file read

--- main.go
+++ main.go
@@ -10,14 +10,30 @@ import (
 func main() {
 	log.SetFlags(0)
 
-	data, err := os.ReadFile("./words.txt")
+	file, err := os.Open("./words.txt")
 	if err != nil {
 		log.Fatalln("failed to read file:", err)
 	}
 
-	wordCount := CountWords(data)
+	PrintFileContents(file)
+}
 
-	fmt.Println(wordCount)
+func PrintFileContents(file *os.File) {
+	const bufferSize = 8192
+	buffer := make([]byte, bufferSize)
+
+	totalSize := 0
+	for {
+		size, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+
+		totalSize += size
+		// fmt.Print(string(buffer[:size]))
+	}
+
+	fmt.Println("total bytes read:", totalSize)
 }
 
 func CountWords(data []byte) int {
